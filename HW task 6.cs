6b5de038e5355dfebf030c65cf1a7e1856fed0f4
Peter Smazhko <smazhko@gmail.com>
2023-03-27 10:48:01 +0300

    домашка 6: подсчёт положительных, пересечение прямых

--- HW task 6.cs
+++ HW task 6.cs
@@ -2,22 +2,25 @@
 //сколько чисел больше 0 ввёл пользователь
 // 0, 7, 8, -2, -2 => 2
 
-/*
-Console.Write("Сколько чисел Вы хотите ввести? ");
-int userCount = Convert.ToInt32(Console.ReadLine());
-int positiveCounter = 0;
 
-for(int i = 1; i <= userCount; i++)
+int CountPositives(int quantity)
 {
-    Console.Write($"Число №{i}: ");
+    int positiveCounter = 0;
+    
+    for(int i = 1; i <= quantity; i++)
+    {
+    Console.Write($"Число №{i} из {quantity}: ");
     if (Convert.ToInt32(Console.ReadLine()) > 0) positiveCounter += 1;
+    }
+
+    return positiveCounter;
 }
 
-if (positiveCounter>0) 
-    Console.WriteLine($"В Вашем числовом ряду положительных чисел: {positiveCounter}.");
-else
-    Console.WriteLine($"В Вашем числовом ряду положительных чисел нет.");
-*/
+Console.Write("Сколько чисел Вы хотите ввести? ");
+int userQuantity = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"В Вашем числовом ряду положительных чисел: {CountPositives(userQuantity)}.");
+
 
 // Напишите программу, которая найдет точку пересечения двух прямых, 
 // заданных уравнениями y = k1 * x + b1 и y = k1 * x + b1. 
@@ -34,6 +37,7 @@ else
 // x * (k1 - k2) = b2 - b1
 // x = (b2 - b1) / (k1 - k2)
 
+/*
 double UserInput(string message)
 {
     Console.Write(message + "... ");
@@ -42,24 +46,29 @@ double UserInput(string message)
     return num;
 }
 
-Console.WriteLine("ПОИСК ТОЧКИ ПЕРЕСЕЧЕНИЯ ДВУХ ПРЯМЫХ НА ПЛОСКОСТИ");
-double k1 = UserInput("Первая прямая y = k1*x + b1. Введите k1");
-double b1 = UserInput("Первая прямая y = k1*x + b1. Введите b1");
-double k2 = UserInput("Вторая прямая y = k2*x + b2. Введите k2");
-double b2 = UserInput("Вторая прямая y = k2*x + b2. Введите b2");
-
-double x = 0;
-double y = 0;
-
-Console.WriteLine();
-
-if (k1 == k2 && b1 == b2)
-    Console.WriteLine("Ответ: данные прямые совпадают.");
-else if (k1 == k2)
-    Console.WriteLine("Ответ: данные прямые параллельны.");
-else
+string IsCross (double k1, double b1, double k2, double b2)
 {
-    x = (b2 - b1) / (k1 - k2);
-    y = (k1 * x + b1);
-    Console.WriteLine($"Ответ: данные прямые пересекаются в точке [{Math.Round(x, 1)}; {Math.Round(y, 1)}].");
+    double x = 0;
+    double y = 0;
+    
+    if (k1 == k2 && b1 == b2)
+        return "совпадают";
+    else if (k1 == k2)
+        return "параллельны";
+    else
+    {
+        x = (b2 - b1) / (k1 - k2);
+        y = (k1 * x + b1);
+        return $"пересекаются в точке [{Math.Round(x, 1)}; {Math.Round(y, 1)}]";
+    }
 }
+
+
+Console.WriteLine("ПОИСК ТОЧКИ ПЕРЕСЕЧЕНИЯ ДВУХ ПРЯМЫХ НА ПЛОСКОСТИ\n");
+double k1 = UserInput("Первая прямая y = k1*x + b1. Введите k1");
+double b1 = UserInput("                             Введите b1");
+double k2 = UserInput("Вторая прямая y = k2*x + b2. Введите k2");
+double b2 = UserInput("                             Введите b2");
+
+Console.WriteLine($"\nОтвет: данные прямые {IsCross (k1, b1, k2, b2)}.");
+*/
